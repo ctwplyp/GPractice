588c2d5a324632a8892c4d93d84aa79e80a83b32
Sean Laing <sean.laing@sample6.com>
2018-01-06 17:05:41 -0800

    f/prac2-scratch-grails-app: Added save functionality - setup controller to save data - setup form to save person data

--- clean/grails-app/controllers/clean/PersonController.groovy
+++ clean/grails-app/controllers/clean/PersonController.groovy
@@ -1,11 +1,17 @@
 package clean
 
-import com.sun.codemodel.internal.JForEach
-
 class PersonController {
 
     def index() {
        def persons = Person.list()
         [persons:persons]
+    }
+    def save() {
+        def person = new Person(params)
+        person.save()
+        render "Success!"
+    }
+    def create(){
+        
     }
 }
