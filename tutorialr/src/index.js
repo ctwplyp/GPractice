11635b45075c345b7e6f72f77b54a9cc77a0f968
Sean Laing <sean.laing@sample6.com>
2018-01-12 18:15:39 -0800

    f/react-rendering: Components and Props - Learning Comnpoents and Props

--- tutorialr/src/index.js
+++ tutorialr/src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+const element = <WelcomeFunc name="SaraFunc" />;
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(
+function WelcomeFunc(props) {
+		return <h1>Hello, {props.name} </h1>;
+};
+
+//class WelcomeComp extends React.Component {
+//	render() {
+//		return <h1>Hello, {this.props.name} </h1>;
+//	}
+//};
+
+//const element1 =  <WelcomeComp name="SaraComp" />;
+
+ReactDOM.render(
     element,
     document.getElementById('root')
-  );
-}
-
-setInterval(tick, 1000);
+);
