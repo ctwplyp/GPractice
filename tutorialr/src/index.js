2bae69031ccd858a3c66daeb6c078cc769982d2e
Sean Laing <sean.laing@sample6.com>
2018-01-12 17:47:12 -0800

    f/react-rendering: Created tick function - implemented tick function in the reactDom - populates time every 1000 ms

--- tutorialr/src/index.js
+++ tutorialr/src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const element = <h1>Hello, world</h1>;
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
+}
 
-ReactDOM.render(
-	element,
-	document.getElementById('root')	
-);
+setInterval(tick, 1000);
