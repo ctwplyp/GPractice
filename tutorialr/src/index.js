2af7a4b0ff24f0b62641f9a3b3b7202c0619ab6e
Sean Laing <sean.laing@sample6.com>
2018-01-15 10:13:14 -0800

    f/react-rendering - Split components - Examples of splitting out components - the run sequence goes comment->userinfo->avatar

--- tutorialr/src/index.js
+++ tutorialr/src/index.js
@@ -1,21 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-const element = <WelcomeFunc name="SaraFunc" />;
+const element = <App name="SaraFunc"  />;
 
 function WelcomeFunc(props) {
 		return <h1>Hello, {props.name} </h1>;
 };
 
-//class WelcomeComp extends React.Component {
-//	render() {
-//		return <h1>Hello, {this.props.name} </h1>;
-//	}
-//};
+function App() {
+  return (
+    <div>
+      <WelcomeFunc name="Sara" />
+      <WelcomeFunc name="Cahal" />
+      <WelcomeFunc name="Edite" />
+    </div>
+  );
+}
 
-//const element1 =  <WelcomeComp name="SaraComp" />;
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+  return (
+    <img
+      className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text:
+    'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl:
+      'http://placekitten.com/g/64/64',
+  },
+};
 
 ReactDOM.render(
-    element,
+     <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+  />,
     document.getElementById('root')
 );
