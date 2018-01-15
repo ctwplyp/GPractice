a4b331dbafa1b8a82e55214d3d16c43b39c57939
Sean Laing <sean.laing@sample6.com>
2018-01-15 15:13:12 -0800

    f/react-rendering: Lists and Keys - Using keys when output values froma  list

--- tutorialr/src/index.js
+++ tutorialr/src/index.js
@@ -3,14 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const numbers = [1, 2, 3, 4, 5];
-const doubled = numbers.map((number) => number * 2);
-const doubledItems = doubled.map((doubled) =>
-  <li>{doubled}</li>
+//const doubledPlus1 = numbers.map((number) => number * 2+1);
+//const doubledItems = doubledPlus1.map((doubled) =>
+ // <li>{doubled}</li>
+//);
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li key={number.toString()}>
+    	{number}
+    </li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+
+ReactDOM.render(
+  <NumberList numbers={numbers} />,
+  document.getElementById('root')
 );
-
-
-
-	ReactDOM.render(
-    	<ul>{doubledItems}</ul>,
-    	document.getElementById('root')
-	);
