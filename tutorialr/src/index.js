b4abb35d671a10ec36278ef0768f58845d8d2e48
Sean Laing <sean.laing@sample6.com>
2018-01-15 15:48:58 -0800

    f/react-rendering: L&K - embedding map in JSX - returned an embedded map from a function to the DOM - JSX allows embedding any expression in curly braces

--- tutorialr/src/index.js
+++ tutorialr/src/index.js
@@ -2,40 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-//const numbers = [1, 2, 3, 4, 5];
+const numbers = [1, 2, 3, 4, 5];
 //const doubledPlus1 = numbers.map((number) => number * 2+1);
 //const doubledItems = doubledPlus1.map((doubled) =>
  // <li>{doubled}</li>
 //);
 
-function Blog(props) {
-const sidebar = (
-    <ul>
-      {props.posts.map((post) =>
-        <li key={post.id}>
-          {post.title}
-        </li>
-      )}
-    </ul>
+function ListItem(props) {
+	return<li>{props.value}</li>;
+}
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  return (
+  	<ul>
+  		{ numbers.map((number) =>
+    		<ListItem key={number.toString()}
+    			value = {number + 4} />
+	)}
+	</ul>
   );
-const content = props.posts.map((post) =>
-	<div key={post.id}>
-	<h3>{post.title}</h3>
-	<p>{post.content}</p>
-	</div>
-	);
-	return (
-	<div>
-		{sidebar}
-		<hr />
-		{content}
-		</div>
-		);
-	}
-const posts = [
-	{id: 1, title: 'Hello World', content: 'Welcome to rails'}];
+}
+
 
 ReactDOM.render(
-  <Blog posts={posts} />,
+  <NumberList numbers={numbers} />,
   document.getElementById('root')
 );
