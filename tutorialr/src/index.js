3e0ef53daa1928ba2953453031b3b67e5d2372ec
Sean Laing <sean.laing@sample6.com>
2018-01-15 14:18:08 -0800

    f/react-rendering: Conditonal Rendering - null - Page that showed a warning until click - had warningBanner send back null if no warning so disappeared

--- tutorialr/src/index.js
+++ tutorialr/src/index.js
@@ -2,84 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Greeting(props) {
-  const isLoggedIn = props.isLoggedIn;
-  if (isLoggedIn) {
-    return <UserGreeting />;
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;
   }
-  return <GuestGreeting />;
-}
 
-function LoginButton(props) {
   return (
-    <button onClick={props.onClick}>
-      Login
-    </button>
-  );
-}
-function UserGreeting(props) {
-  return <h1>Welcome back!</h1>;
-}
-
-function GuestGreeting(props) {
-  return <h1>Please sign up.</h1>;
-}
-
-
-function LogoutButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Logout
-    </button>
+    <div className="warning">
+      Warning!
+    </div>
   );
 }
 
- class LoginControl extends React.Component {
+class Page extends React.Component {
   constructor(props) {
     super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    this.state = {isLoggedIn: false};
+    this.state = {showWarning: true}
+    this.handleToggleClick = this.handleToggleClick.bind(this);
   }
 
-  handleLoginClick() {
-    this.setState({isLoggedIn: true});
-  }
-
-  handleLogoutClick() {
-    this.setState({isLoggedIn: false});
+  handleToggleClick() {
+    this.setState(prevState => ({
+      showWarning: !prevState.showWarning
+    }));
   }
 
   render() {
-    const isLoggedIn = this.state.isLoggedIn;
- 	return (	
+    return (
       <div>
-    {isLoggedIn ? (
-      <LogoutButton onClick={this.handleLogoutClick} />
-    ) : (
-      <LoginButton onClick={this.handleLoginClick} />
-    )}
-    	The user is <b>{isLoggedIn ? "currently" : 'not'}</b> logged in.
-    </div>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? 'Hide' : 'Show'}
+        </button>
+      </div>
     );
+  }
 }
-}
-	function Mailbox(props) {
-		const unreadMessages = props.unreadMessages;
-  			return (
-    			<div>
-      			<h1>Hello!</h1>
-      			{unreadMessages.length > 0 &&
-        		<h2>
-          		You have {unreadMessages.length} unread messages.
-        		</h2>
-      			}
-    		</div>
-  		);
-	}
 
-	const messages= ['React', 'Re: React', 'Re:Re: React','Reactsdf'];
 	ReactDOM.render(
-    	<LoginControl />,
+    	<Page />,
     	document.getElementById('root')
 	);
