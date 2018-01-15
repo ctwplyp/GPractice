a8128beedff7300bc605ab44dab250d517596047
Sean Laing <sean.laing@sample6.com>
2018-01-15 14:44:57 -0800

    f/react-rendering: Lists and Keys - functional doubling numbers in a list - functional outputting the doubled numbers

--- tutorialr/src/index.js
+++ tutorialr/src/index.js
@@ -2,44 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function WarningBanner(props) {
-  if (!props.warn) {
-    return null;
-  }
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map((number) => number * 2);
+const doubledItems = doubled.map((doubled) =>
+  <li>{doubled}</li>
+);
 
-  return (
-    <div className="warning">
-      Warning!
-    </div>
-  );
-}
 
-class Page extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {showWarning: true}
-    this.handleToggleClick = this.handleToggleClick.bind(this);
-  }
-
-  handleToggleClick() {
-    this.setState(prevState => ({
-      showWarning: !prevState.showWarning
-    }));
-  }
-
-  render() {
-    return (
-      <div>
-        <WarningBanner warn={this.state.showWarning} />
-        <button onClick={this.handleToggleClick}>
-          {this.state.showWarning ? 'Hide' : 'Show'}
-        </button>
-      </div>
-    );
-  }
-}
 
 	ReactDOM.render(
-    	<Page />,
+    	<ul>{doubledItems}</ul>,
     	document.getElementById('root')
 	);
