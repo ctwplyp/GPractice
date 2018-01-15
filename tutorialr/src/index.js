77224ec91b07560b8dc707496eb4bda82dc2379f
Sean Laing <sean.laing@sample6.com>
2018-01-15 10:48:57 -0800

    f/react-rendering: Going form functional call to class

--- tutorialr/src/index.js
+++ tutorialr/src/index.js
@@ -1,77 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-const element = <App name="SaraFunc"  />;
 
-function WelcomeFunc(props) {
-		return <h1>Hello, {props.name} </h1>;
-};
-
-function App() {
-  return (
-    <div>
-      <WelcomeFunc name="Sara" />
-      <WelcomeFunc name="Cahal" />
-      <WelcomeFunc name="Edite" />
-    </div>
-  );
+class Clock extends React.Component {
+	render() {
+		return (
+			<div>
+					<h1>Hello, world!</h1>
+					<h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+			</div>
+		);
+	}
 }
 
-function formatDate(date) {
-  return date.toLocaleDateString();
+function tick() {
+	ReactDOM.render(
+    	<Clock date={new Date()} />,
+    	document.getElementById('root')
+	);
 }
 
-function Avatar(props) {
-  return (
-    <img
-      className="Avatar"
-      src={props.user.avatarUrl}
-      alt={props.user.name}
-    />
-  );
-}
-
-function UserInfo(props) {
-  return (
-    <div className="UserInfo">
-      <Avatar user={props.user} />
-      <div className="UserInfo-name">
-        {props.user.name}
-      </div>
-    </div>
-  );
-}
-
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <UserInfo user={props.author} />
-      <div className="Comment-text">
-        {props.text}
-      </div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
-    </div>
-  );
-}
-
-const comment = {
-  date: new Date(),
-  text:
-    'I hope you enjoy learning React!',
-  author: {
-    name: 'Hello Kitty',
-    avatarUrl:
-      'http://placekitten.com/g/64/64',
-  },
-};
-
-ReactDOM.render(
-     <Comment
-    date={comment.date}
-    text={comment.text}
-    author={comment.author}
-  />,
-    document.getElementById('root')
-);
+setInterval(tick, 1000)
