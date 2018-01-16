1ceeb82631079de36bcc99609a2e65d719864802
Sean Laing <sean.laing@sample6.com>
2018-01-16 06:18:21 -0800

    f/Forms - Form with file inputs

--- tutorialr/src/index.js
+++ tutorialr/src/index.js
@@ -2,43 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class EssayForm extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: 'Please write an essay about your favorite Dom element.'};
+class FileInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(
+      this
+    );
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    alert(
+      'Selected file - ${this.fileInput.files[0].name}'
+    );
+  }
 
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-		
-	}
-
-	handleChange(event) {
-		//only uppercase letters allowed
-		this.setState({value: event.target.value.toUpperCase()});
-	}
-
-	handleSubmit(event) {
-		alert('A name was submitted: ' + this.state.value);
-		event.preventDefault();
-	}
-
-	render() {
-		return (
-			<form onSubmit={this.handleSubmit}>
-				<label>
-					Essay:
-					<textarea value={this.state.value} onChange={this.handleChange} />
-				</label>
-				<input type="submit" value="Submit" />
-				</form>
-			);
-	}
-
+  render() {
+    return (
+      <form
+        onSubmit={this.handleSubmit}>
+        <label>
+          Upload file:
+          <input
+            type="file"
+            ref={input => {
+              this.fileInput = input;
+            }}
+          />
+        </label>
+        <br />
+        <button type="submit">
+          Submit
+        </button>
+      </form>
+    );
+  }
 }
 
-
 ReactDOM.render(
-  <EssayForm />,
+  <FileInput />,
   document.getElementById('root')
 );
