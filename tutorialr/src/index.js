aaae6e9989936fdade7e379c419262f9fd2a2d8a
Sean Laing <sean.laing@sample6.com>
2018-01-23 09:42:04 -0800

    f/jsx-in-depth: Undefined variables output - Passing different params in - Outputing an undefined variable - Using children props to output values

--- tutorialr/src/index.js
+++ tutorialr/src/index.js
@@ -3,35 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-function NumberDescriber(props) {
-  let description;
-  if (props.number % 2 == 0) {
-    description =<strong>even</strong>
-  }else {
-    description = <i>odd</i>;
-  }
-  return <div>{props.number} is an {description} number </div>
-}
-const showHeader =true;
-
-function TwoNumber(props1){
-const props = {number: props1.number2}
-  return <div> 
-          <NumberDescriber number={props1.number1} />
-          <div>{showHeader && <Header messages={"{props1.number1}"}/>}</div>
-           <NumberDescriber number={props1.number2} />
-           <NumberDescriber {...props} />
-           <NumberDescriber></NumberDescriber>
-           </div>
-}
-
-function Header(props){
-  return <div>{props.messages && "true" 
-              //  <MessageList messages={props.messages}/>
-              }
-         </div>
-}
-
 
 function Repeat(props) {
   let items =[];
@@ -42,9 +13,11 @@ function Repeat(props) {
 }
 
 function ListOfTenThings() {
+  var myVariable
   return (
     <Repeat numTimes={10}>
-      {(index) => <div key={index}><div>{showHeader && <Header />}</div>This is item {index} in the list</div>}
+      {(index) => <div key={index}><div>
+      </div>My JavaScript {index} variable is {String(myVariable)}</div>}
       </Repeat>
       )
 }
