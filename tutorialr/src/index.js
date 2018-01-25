7773d0a6fc77391dc0b4d4cb48a302ff2a40fc81
Sean Laing <sean.laing@sample6.com>
2018-01-25 11:01:51 -0800

    f/redux: fulfilled data requests - using promises got data - save data to the state

--- tutorialr/src/index.js
+++ tutorialr/src/index.js
@@ -6,7 +6,6 @@ import thunk from "redux-thunk"
 import {applyMiddleware, createStore} from "redux";
 import logger from "redux-logger"
 import promise from "redux-promise-middleware"
-import {combineReducers } from "redux";
 
 
 class HelloMessage extends React.Component {
@@ -25,60 +24,7 @@ const initialState ={
   error: null,
 }
 
-
-
-const tweetsReducer = (state={
-  tweets: [],
-  fetching: false,
-  fetched: false,
-  error: null,}, action) => {
-  switch (action.type){
-    case "FETCH_TWEETS": {
-      return {...state, fetching:true}
-        break;
-    }
-    case "FETCH_TWEETS_REJECTED" : {
-      return {...state, fetching:false, error: action.payload}
-        break;
-    }
-    case "FETCH_TWEETS__FULFILLED" : {
-      return {...state,
-              fetching: false,
-              fetched: true,
-              users: action.payload,
-            }
-      break
-    }
-    case "ADD_TWEET" : {
-      return {...state,
-              fetching: false,
-              fetched: true,
-              users: action.payload,
-            }
-      break
-    }
-    case "UPDATE_TWEET": {
-      const {id, text } = action.payload
-      const newTweets = [...state.tweets]
-      const tweetToUpdate = newTweets.findIndex(tweet => tweet.id ===id)
-      newTweets[tweetToUpdate] = action.payload
-      return {
-      ...state,
-      tweets: newTweets,}
-      
-    }
-    case "DELETE_TWEET": {
-      return {
-        ...state,
-        tweets: state.tweets.filter(tweet => tweet.id !== action.payload),
-      }
-    }
-  }
-  return state
-}
-
-
-const userReducer = (state=initialState, action) => {
+const reducer = (state=initialState, action) => {
   switch (action.type){
     case "FETCH_USERS_PENDING": {
       return {...state, fetching:true}
@@ -103,14 +49,9 @@ const userReducer = (state=initialState, action) => {
   return state
 }
 
-const reducers = combineReducers({
-  user: userReducer,
-  tweets: tweetsReducer,
-})
-
 const middleware = applyMiddleware(promise(), thunk, logger);
 
-const store = createStore(reducers, 1, middleware);
+const store = createStore(reducer, 1, middleware);
 
 store.dispatch({
   type:"FETCH_USERS",
