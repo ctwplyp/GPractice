ac2bfb2e6498b60fdc0761aa69529e9c116630f9
ctwplyp <ctwplyp@gmail.com>
2018-02-26 14:50:44 -0800

    f/JavaSetup: added node for practicing linkedlists

---
+++ java/interviewPrep/src/Student.java
@@ -0,0 +1,26 @@
+public class Student {
+    String name;
+    Integer id;
+
+    public Student(String name, Integer id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+}
