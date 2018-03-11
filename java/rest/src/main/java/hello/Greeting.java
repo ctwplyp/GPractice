b567ce88b59720d62e856d0cdab0a6d0f5baec8e
ctwplyp <ctwplyp@gmail.com>
2018-03-11 12:49:45 -0700

    f/JavaSetup: Added Fizzbuz to greeting api - Focus on doing it functionally - Looking to come back it do it a couple different ways to practice java 8 functional

--- java/rest/src/main/java/hello/Greeting.java
+++ java/rest/src/main/java/hello/Greeting.java
@@ -1,5 +1,10 @@
 package hello;
 
+import java.util.Collections;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Greeting {
 
     private final long id;
@@ -15,6 +20,17 @@ public class Greeting {
     }
 
     public String getContent() {
-        return content;
+        return fizzBuzz8();
+        //return content;
+    }
+
+    public static String fizzBuzz8() {
+       // String[] answer = new String[100];
+        return  IntStream.range(1,100)
+                .mapToObj( n -> n % 3 == 0 ?
+                        ( n % 5 == 0 ? "FizzBuzz" : "Fizz") :
+                        ( n % 5 == 0 ? "Buzz" :n))
+                .map(n -> n.toString()).collect(Collectors.joining(","));
+
     }
 }
